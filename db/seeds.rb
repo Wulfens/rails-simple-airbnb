2209abe1eccad6b6ef2dc42f10c511850f46df59
Mathieu Trancoso <trancoso.mathieu@gmail.com>
2021-02-10 18:00:55 +0100

    finito

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+)
+Flat.create!(
+  name: 'Stylish House Close to River Thames',
+  address: 'Covent Garden',
+  description: 'A lovely spacious flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 112,
+  number_of_guests: 2,
+)
+Flat.create!(
+  name: 'Bryan Nakache',
+  address: 'City Hall',
+  description: 'Best place in a trendy area',
+  price_per_night: 190,
+  number_of_guests: 4,
+)
+Flat.create!(
+  name: 'Small trendy flat',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 50,
+  number_of_guests: 2,
+)
+Flat.create!(
+  name: 'Garden and spacious living room',
+  address: 'Out of London',
+  description: 'Best house in a middle of nature',
+  price_per_night: 200,
+  number_of_guests: 6,
+)
